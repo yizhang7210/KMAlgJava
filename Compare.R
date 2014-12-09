637779756d23673a1516c9fd8e228e80158f126d
Yi Zhang <yi.zhang7210@gmail.com>
2014-12-09 16:54:36 -0500

    More tests.....

--- Compare.R
+++ Compare.R
@@ -1,8 +1,8 @@
-origPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/TestFromFourier.csv';
-estiPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/TestEstimated.csv';
+#origPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/TestFromFourier.csv';
+#estiPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/TestEstimated.csv';
 
-#origPath <- '/home/y825zhan/00ME/CS860/JavaImp/Test.csv';
-#estiPath <- '/home/y825zhan/00ME/CS860/JavaImp/TestEstimated.csv';
+origPath <- '/home/y825zhan/00ME/CS860/JavaImp/TestFromFourier.csv';
+estiPath <- '/home/y825zhan/00ME/CS860/JavaImp/TestEstimated.csv';
 
 origTable <- as.matrix(read.csv(origPath, sep = "", header = F, skip = 1));
 estiTable <- as.matrix(read.csv(estiPath, sep = "", header = F, skip = 1));
@@ -14,7 +14,7 @@ range = 1:noObs;
 
 ord = order(origTable[,n+1], decreasing=T);
 origVals = origTable[,n+1][ord];
-estiVals = estiTable[,n+1][ord];
+estiVals = estiTable[,n+1][order(estiTable[,n+1], decreasing=T)];
 
 # The estimation, h(x):
 plot(estiVals[range], type = 'l', xlab = 'x', ylab = 'h(x)', col = 4);
@@ -23,16 +23,23 @@ plot(estiVals[range], type = 'l', xlab = 'x', ylab = 'h(x)', col = 4);
 lines(origVals[range], col = 2);
 
 # Titles and legends and others:
-legend('bottomright', legend = c("Estimated h(x)", "Real f(x)"), 
+legend('topright', legend = c("Estimated h(x)", "Real f(x)"), 
        lwd = c(2.5, 2.5), col = c(4,2));
-error <- mean(abs((origTable[, n+1] - estiTable[, n+1])/origTable[,n+1]));
-error2 <- sum((origTable[, n+1] - estiTable[, n+1])^2)
+errors <- abs(origTable[, n+1] - estiTable[, n+1])/abs(origTable[,n+1]);
+maxerror <- max(errors);
+minerror <- min(errors);
+error <- mean(errors);
+error2 <- sum((origTable[, n+1] - estiTable[, n+1])^2)/(2^n);
 
 #=====================================================================
 # Comparing Fourier coefficients
 
-origCoefPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/origFouriers.csv';
-estiCoefPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/TestFourierEstimated.csv';
+#origCoefPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/origFouriers.csv';
+#estiCoefPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/TestFourierEstimated.csv';
+
+origCoefPath <- '/home/y825zhan/00ME/CS860/JavaImp/origFouriers.csv';
+estiCoefPath <- '/home/y825zhan/00ME/CS860/JavaImp/TestFourierEstimated.csv';
+
 
 origCoefs<- as.matrix(read.csv(origCoefPath, sep = "", header = F, skip = 1));
 estiCoefs <- as.matrix(read.csv(estiCoefPath, sep = "", header = F, skip = 1));
--- src/RunKMAlg.java
+++ src/RunKMAlg.java
@@ -33,26 +33,27 @@ public class RunKMAlg {
         
         //FourierTester.GenerateTestToFile(tmp, 8, 200);
         
-        int n = 14;
-        double ep = 0.12, del = 0.1;
+        int n = 18;
+        int t = 20;
+        double ep = 0.02, del = 0.25;
       
         int numSample = (int) Math.ceil(2/(ep*ep)*((n+1)*Math.log(2) + Math.log(1/del)));
         //int numSample = (int) Math.ceil(Math.pow(2, n));
         
-        double err = Math.pow(2, n)*ep*ep;
+        double err = t*ep*ep;
         
         System.out.println("Guanranteed error is: " + err);
         System.out.println("Total number of combinations is: " + Math.pow(2, n));
         System.out.println("Number of samples is: " + numSample);
 
-        FourierTester.GenerateTestFromFourierToFile(tmp, n, numSample, 50);
+        FourierTester.GenerateTestFromFourierToFile(tmp, n, numSample, t);
         
         //FourierLearner K = new FourierLearner("TestSys", tmp);
         //FourierLearner K = new FourierLearner("Apache", tmp);
         
         TrivialLearner L = new TrivialLearner("TestSys", tmp);
         
-        SimpleMatrix fCoefs = L.learn(numSample, 50, true);
+        SimpleMatrix fCoefs = L.learn(numSample, t, true);
         
         String tmp2 = "TestFourierEstimated.csv";
         try{
