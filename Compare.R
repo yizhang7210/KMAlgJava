61ab01926770f926182407abb9e5a671cb6083e8
Yi Zhang <yi.zhang7210@gmail.com>
2014-12-11 18:18:20 -0500

    Lots of weird things.

--- Compare.R
+++ Compare.R
@@ -1,16 +1,44 @@
-origPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/OrigTestFun.csv';
-estiPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/TestEstimated.csv';
+isTest = F;
 
-#origPath <- '/home/y825zhan/00ME/CS860/JavaImp/TestFromFourier.csv';
-#estiPath <- '/home/y825zhan/00ME/CS860/JavaImp/TestEstimated.csv';
+if(isTest){
+  #origPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/OrigTestFun.csv';
+  #estiPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/TestEstimated.csv';
+  
+  origPath <- '/home/y825zhan/00ME/CS860/JavaImp/OrigTestFun.csv';
+  estiPath <- '/home/y825zhan/00ME/CS860/JavaImp/TestEstimated.csv';
+  
+  #origCoefPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/OrigCoefs.csv';
+  #estiCoefPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/TestCoefsEstimated.csv';
+  
+  origCoefPath <- '/home/y825zhan/00ME/CS860/JavaImp/OrigCoefs.csv';
+  estiCoefPath <- '/home/y825zhan/00ME/CS860/JavaImp/TestCoefsEstimated.csv';
+  
+}else{
+  #origPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/OrigTestFun.csv';
+  #estiPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/TestEstimated.csv';
+  
+  origPath <- '/home/y825zhan/00ME/CS860/JavaImp/X264Processed.csv';
+  estiPath <- '/home/y825zhan/00ME/CS860/JavaImp/X264Estimated.csv';
+  
+  #origCoefPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/OrigCoefs.csv';
+  #estiCoefPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/TestCoefsEstimated.csv';
+  
+  origCoefPath <- '/home/y825zhan/00ME/CS860/JavaImp/OrigCoefs.csv';
+  estiCoefPath <- '/home/y825zhan/00ME/CS860/JavaImp/TestCoefsEstimated.csv';
+  
+  origPath <- '/home/y825zhan/00ME/CS860/JavaImp/X264Complete.csv';
+  estiPath <- '/home/y825zhan/00ME/CS860/JavaImp/X264CompleteEstimated.csv';
+}
 
+
+#==============================================================================
 origTable <- as.matrix(read.csv(origPath, sep = "", header = F, skip = 1));
 estiTable <- as.matrix(read.csv(estiPath, sep = "", header = F, skip = 1));
 
 n <- ncol(origTable) - 1;
 noObs <- nrow(origTable);
 
-range = 1:300;
+range = 1:noObs;
 
 ord = order(origTable[,n+1], decreasing=T);
 origVals = origTable[,n+1][ord];
@@ -22,7 +50,7 @@ estiVals = estiTable[,n+1][ord];
 minVal = min(estiVals[range], origVals[range]);
 maxVal = max(estiVals[range], origVals[range]);
 
-plot(estiVals[range], type = 'l', xlab = 'x', ylab = 'h(x)', col = 4, 
+plot(estiVals[range]*noObs/2^n, type = 'l', xlab = 'x', ylab = 'h(x)', col = 4,
      ylim = c(minVal*0.95,maxVal*1.05));
 
 # The real, f(x):
@@ -31,7 +59,7 @@ lines(origVals[range], col = 2);
 # Titles and legends and others:
 legend('topright', legend = c("Estimated h(x)", "Real f(x)"), 
        lwd = c(2.5, 2.5), col = c(4,2));
-errors <- as.matrix(abs(origTable[, n+1] - estiTable[, n+1])/abs(origTable[,n+1]));
+errors <- as.matrix(abs(origTable[, n+1] - estiTable[, n+1]*noObs/2^n)/abs(origTable[,n+1]));
 maxerror <- max(errors);
 minerror <- min(errors);
 error <- mean(errors);
@@ -40,37 +68,30 @@ error2 <- sum((origTable[, n+1] - estiTable[, n+1])^2)/(2^n);
 plot(errors[range], type='l', xlab = 'x', ylab = 'error at x', col = 4);
 
 
-#=====================================================================
-# Comparing Fourier coefficients
-
-origCoefPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/OrigCoefs.csv';
-estiCoefPath <- '/home/yzhang/00ME/Education/UW/CS860/JavaImp/TestCoefsEstimated.csv';
-
-#origCoefPath <- '/home/y825zhan/00ME/CS860/JavaImp/origFouriers.csv';
-#estiCoefPath <- '/home/y825zhan/00ME/CS860/JavaImp/TestFourierEstimated.csv';
-
-
-origCoefs<- as.matrix(read.csv(origCoefPath, sep = "", header = F, skip = 1));
-estiCoefs <- as.matrix(read.csv(estiCoefPath, sep = "", header = F, skip = 1));
-
-origCoefs <- origCoefs[do.call(order, as.data.frame(origCoefs)),]
-estiCoefs <- estiCoefs[do.call(order, as.data.frame(estiCoefs)),]
-
-# # Order coefs may not make sense
-# coefOrd <- order(origCoefs[,n+1],decreasing = T);
-# origCoefs <- origCoefs[coefOrd,]
-# estiCoefs <- estiCoefs[coefOrd,]
-
-
-#================================================
-# Plot
-plot(estiCoefs[,n+1], type = 'l', xlab = 'x', ylab = 'h(x)', col = 4);
-
-# The real, f(x):
-lines(origCoefs[,n+1], col = 2);
-
-# Titles and legends and others:
-legend('bottomleft', legend = c("Estimated h(x)", "Real f(x)"), 
-       lwd = c(2.5, 2.5), col = c(4,2));
+# #=====================================================================
+# # Comparing Fourier coefficients
+# 
+# origCoefs<- as.matrix(read.csv(origCoefPath, sep = "", header = F, skip = 1));
+# estiCoefs <- as.matrix(read.csv(estiCoefPath, sep = "", header = F, skip = 1));
+# 
+# origCoefs <- origCoefs[do.call(order, as.data.frame(origCoefs)),]
+# estiCoefs <- estiCoefs[do.call(order, as.data.frame(estiCoefs)),]
+# 
+# # # Order coefs may not make sense
+# # coefOrd <- order(origCoefs[,n+1],decreasing = T);
+# # origCoefs <- origCoefs[coefOrd,]
+# # estiCoefs <- estiCoefs[coefOrd,]
+# 
+# 
+# #================================================
+# # Plot
+# plot(estiCoefs[,n+1], type = 'l', xlab = 'x', ylab = 'h(x)', col = 4);
+# 
+# # The real, f(x):
+# lines(origCoefs[,n+1], col = 2);
+# 
+# # Titles and legends and others:
+# legend('bottomleft', legend = c("Estimated h(x)", "Real f(x)"), 
+#        lwd = c(2.5, 2.5), col = c(4,2));
 
 
--- src/RunKMAlg.java
+++ src/RunKMAlg.java
@@ -19,15 +19,14 @@ public class RunKMAlg {
         long startTime = System.currentTimeMillis();
 
         String [] systems = {"Apache", "X264", "LLVM"};
-        String [] sampleLocs = {
-            "/home/yzhang/00ME/Education/UW/CS860/JavaImp/ApacheProcessed.csv",
-            "/home/yzhang/00ME/Education/UW/CS860/JavaImp/X264Processed.csv",
-            "/home/yzhang/00ME/Education/UW/CS860/JavaImp/LLVMProcessed.csv"};
+        
+        String [] sampleLocs = {"ApacheProcessed.csv", 
+            "X264Processed.csv", "LLVMProcessed.csv"};
         
         
-        RunKMAlg.runOnTest(16, 0.05, 0.1, 40);
+        //RunKMAlg.runOnTest(18, 0.05, 0.1, 50);
         
-        //RunKMAlg.runOnData(systems[0], sampleLocs[0], 50, 5);
+        RunKMAlg.runOnData(systems[1], sampleLocs[1], 600, 0.05);
         
         
         // End timer:
@@ -57,8 +56,8 @@ public class RunKMAlg {
 
         TrivialLearner L = new TrivialLearner("TestSys", origFun);
         
-        //double[][] fCoefs = L.learn(numSample, theta, true);
-        double[][] fCoefs = L.oldLearn(numSample, t+3, true);
+        double[][] fCoefs = L.learn(numSample, 1.0/t, true);
+        //double[][] fCoefs = L.oldLearn(numSample, t, true);
         
         Matrix.write(fCoefs, estiCoef);
         
@@ -78,13 +77,14 @@ public class RunKMAlg {
         TrivialLearner L = new TrivialLearner(sysName, sampleLoc);
         
         double[][] fCoefs = L.learn(numSample, theta, true);
-        //double[][] fCoefs = L.oldLearn(numSample, t, true);
+        //double[][] fCoefs = L.oldLearn(numSample, (int) theta, true);
         
         Matrix.write(fCoefs, estiCoef);
         
         FourierResult R = new FourierResult(fCoefs);
         
         R.estimateAllSample(estiFun, sampleLoc);
+        R.estimateAllSample(sysName+"CompleteEstimated.csv", sysName+"Complete.csv");
         
         Matrix.print(fCoefs);      
     }
