70cf0e8fdc329e208acbb0f6e5bb6986cb73fc99
Yi Zhang <yi.zhang7210@gmail.com>
2014-12-12 01:26:56 -0500

    Forgot to same .R

--- Compare.R
+++ Compare.R
@@ -28,9 +28,15 @@ estiCoefPath <- 'TestCoefsEstimated.csv';
 #==============================================================================
 origTable <- as.matrix(read.csv(origPath, sep = "", header = F, skip = 1));
 estiTable <- as.matrix(read.csv(estiPath, sep = "", header = F, skip = 1));
+
+n <- ncol(origTable) - 1;
+noObs <- nrow(origTable);
+
+
+#=============================================================================
+# Checking how it does outside of the sample
 origAll <- as.matrix(read.csv(origAllPath, sep = "", header = F, skip = 1));
 estiAll <- as.matrix(read.csv(estiAllPath, sep = "", header = F, skip = 1));
-
 
 origNon <- rbind(origTable[,1:n], origAll[,1:n]);
 estiNon <- rbind(estiTable[,1:n], estiAll[,1:n]);
@@ -43,9 +49,9 @@ plot(estiNon[,n+1], type='l');
 
 
 
-n <- ncol(origTable) - 1;
-noObs <- nrow(origTable);
 
+#==========================================================
+# Checking how it does inside the sample
 range = 1:noObs;
 
 ord = order(origTable[,n+1], decreasing=T);
@@ -76,30 +82,30 @@ error2 <- sum((origTable[, n+1] - estiTable[, n+1])^2)/(2^n);
 plot(errors[range], type='l', xlab = 'x', ylab = 'error at x', col = 4);
 
 
-# #=====================================================================
-# # Comparing Fourier coefficients
-# 
-# origCoefs<- as.matrix(read.csv(origCoefPath, sep = "", header = F, skip = 1));
-# estiCoefs <- as.matrix(read.csv(estiCoefPath, sep = "", header = F, skip = 1));
-# 
-# origCoefs <- origCoefs[do.call(order, as.data.frame(origCoefs)),]
-# estiCoefs <- estiCoefs[do.call(order, as.data.frame(estiCoefs)),]
-# 
-# # # Order coefs may not make sense
-# # coefOrd <- order(origCoefs[,n+1],decreasing = T);
-# # origCoefs <- origCoefs[coefOrd,]
-# # estiCoefs <- estiCoefs[coefOrd,]
-# 
-# 
-# #================================================
-# # Plot
-# plot(estiCoefs[,n+1], type = 'l', xlab = 'x', ylab = 'h(x)', col = 4);
-# 
-# # The real, f(x):
-# lines(origCoefs[,n+1], col = 2);
-# 
-# # Titles and legends and others:
-# legend('bottomleft', legend = c("Estimated h(x)", "Real f(x)"), 
-#        lwd = c(2.5, 2.5), col = c(4,2));
+#=====================================================================
+# Comparing Fourier coefficients
+
+origCoefs<- as.matrix(read.csv(origCoefPath, sep = "", header = F, skip = 1));
+estiCoefs <- as.matrix(read.csv(estiCoefPath, sep = "", header = F, skip = 1));
+
+origCoefs <- origCoefs[do.call(order, as.data.frame(origCoefs)),]
+estiCoefs <- estiCoefs[do.call(order, as.data.frame(estiCoefs)),]
+
+# # Order coefs may not make sense
+# coefOrd <- order(origCoefs[,n+1],decreasing = T);
+# origCoefs <- origCoefs[coefOrd,]
+# estiCoefs <- estiCoefs[coefOrd,]
+
+
+#================================================
+# Plot
+plot(estiCoefs[,n+1], type = 'l', xlab = 'x', ylab = 'h(x)', col = 4);
+
+# The real, f(x):
+lines(origCoefs[,n+1], col = 2);
+
+# Titles and legends and others:
+legend('bottomleft', legend = c("Estimated h(x)", "Real f(x)"), 
+       lwd = c(2.5, 2.5), col = c(4,2));
 
 
